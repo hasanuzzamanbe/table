69e48a2ef57ed93d44ec7bf850be01b069f52928
hasanuzzamanbe <43160844+hasanuzzamanbe@users.noreply.github.com>
2018-10-23 16:33:39 +0600

    static table created

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,45 @@
 <template>
-  <div class="home">
-    good to see you home
+  <div style="width: 900px;">
+    <el-table
+      :data="[]">
+      <el-table-column
+        v-for="(column, columnIndex) in tableSettings" :key="columnIndex"
+        :prop="column.key"
+        :label="column.name"
+        width="180">
+      </el-table-column>
+    </el-table>
   </div>
-
 </template>
 
 <script>
-export default {
-  name: 'Home',
-  data () {
-    return {
-
+  export default {
+    data () {
+      return {
+        tableSettings: [
+        {
+            name: 'First Name',
+            key: 'first_name',
+            type: 'single',
+            breakpoint: 'a-s'
+        },
+        {
+            name: 'Last Name',
+            key: 'last_name',
+            type: 'single',
+            breakpoint: 'a-s'
+        },
+        {
+            name: 'Age',
+            key: 'age',
+            type: 'single',
+            breakpoint: 'a-s'
+        }
+      ]
+      }
+    },
+    methods: {
+      
     }
   }
-}
 </script>
-
-<style>
-  
-
-
-
-</style>
-
--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// import Vuetify from 'vuetify'
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en'
 
+import 'element-ui/lib/theme-chalk/index.css';
+// import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(ElementUI, { locale });
+// Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
