d1cf6d32ff243e226185c2e6558d4e6aad0a432b
hasanuzzamanbe <43160844+hasanuzzamanbe@users.noreply.github.com>
2018-10-23 10:39:24 +0600

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    good to see you home
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+  
+
+
+
+</style>
+
